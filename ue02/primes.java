e86ccac86185a537897eae1cf9b24fdcef931afe
sahin <sahin@cip.ifi.lmu.de>
2017-07-11 18:22:51 +0200

    Übung 02

---
+++ ue02/primes.java
@@ -0,0 +1,41 @@
+/**
+ * Created by sahin on 11.07.17.
+ */
+public class primes {
+
+    public static boolean[] primes (int n) {
+
+        boolean[] isPrime = new boolean[n];
+
+        for (int i = 0; i < isPrime.length; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i < isPrime.length; i++) {
+
+            if(isPrime[i] == true) {
+                int currentPrime = i;
+                int index = currentPrime + i;
+                while (index < n) {
+                    isPrime[index] = false;
+                    index += currentPrime;
+                }
+            }
+
+        }
+
+        return isPrime;
+
+    }
+
+    public static void main(String[] args) {
+
+        boolean[] primeValues = primes(1000);
+
+        for (int i = 0; i < primeValues.length; i++) {
+            System.out.printf("%1$d is a prime:\t%2$b \n", i, primeValues[i]);
+        }
+
+    }
+
+}
